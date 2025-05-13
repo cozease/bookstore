a7027530a13878e447de60e05159d19fdd8bc6cb
cozease <cozease_y@sjtu.edu.cn>
2025-05-13 16:35:40 +0800

    init

---
+++ src/test/java/me/cozease/bookstorebackend/BookstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.cozease.bookstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
